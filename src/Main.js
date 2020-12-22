0c0890b4dc4805906fa9f839c24a410c1ef3d5d7
jo-turn <94jhgmsg@gmail.com>
2020-12-22 17:50:57 +0900

    client base setting

---
+++ src/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import MainMy from './MainMy'
+import MainPublic from './MainPublic'
+import Content from './Content'
+
+const Main = () => {
+  return <div></div>
+}
+
+export default Main
