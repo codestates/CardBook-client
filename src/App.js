9067d9df3292e17fc500a2b1d0866c5228b25517
hoon <iamleonardjeong@gmail.com>
2020-12-22 18:00:15 +0900

    css writing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import sample from "./output.mp4";
+import "./App.css";
+
 function App() {
-  return <div>Hello World!!!</div>;
+  return (
+    <div className="container">
+      <div className="left">
+        <video className="videoTag" autoPlay loop muted>
+          <source src={sample} type="video/mp4" />
+        </video>
+      </div>
+      <div className="right">
+        <h1>CardBook</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
