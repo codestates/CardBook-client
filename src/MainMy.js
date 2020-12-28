4bbe0364553a507befad24b01b74d5add5b3ed0a
jo-turn <94jhgmsg@gmail.com>
2020-12-28 11:24:49 +0900

    2020 1228 Component Refactory

--- src/MainMy.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const MainMy = () => {
-  return <div></div>
-}
-
-export default MainMy
---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Auth from 'routers/Auth'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Auth />
+      </div>
+    )
+  }
+}
+
+export default App
