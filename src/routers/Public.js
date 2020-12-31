2a847e6356e40a2effc1b09806c381697d85072f
jo-turn <94jhgmsg@gmail.com>
2020-12-31 15:19:39 +0900

    update

--- src/routers/Public.js
+++ src/routers/Public.js
@@ -1,35 +1,35 @@
-import React, { useEffect, useState } from "react";
-import "routers/Public.css";
-import PublicCard from "components/PublicCard";
-import axios from "axios";
+import React, { useEffect, useState } from 'react'
+import 'routers/Public.css'
+import PublicCard from 'components/PublicCard'
+import axios from 'axios'
 
 const Public = () => {
-  const [movies, setMovies] = useState([]);
-  const [contents, setContents] = useState([]);
+  const [movies, setMovies] = useState([])
+  const [contents, setContents] = useState([])
 
   useEffect(() => {
     const getContentsData = async () => {
       let data = await axios.get(
-        "https://www.cardbookserver.tk:4000/contents/viewpubliclists"
-      );
-      setContents(data.data);
-    };
-    getContentsData();
-  }, []);
+        'https://www.cardbookserver.tk:4000/contents/viewpubliclists'
+      )
+      setContents(data.data)
+    }
+    getContentsData()
+  }, [])
 
   return (
     <>
       <div className="cardContainor">
         {contents.map((content, index) => {
           if (index < 200) {
-            return <PublicCard key={content.id} content={content} />;
+            return <PublicCard key={content.id} content={content} />
           } else {
-            return;
+            return console.log(index)
           }
         })}
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Public;
+export default Public
