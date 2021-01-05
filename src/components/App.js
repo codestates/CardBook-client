2e8f31a26e2b88068f0c6b3c7efa1b79c6dc915b
hoon <iamleonardjeong@gmail.com>
2021-01-05 13:18:33 +0900

    add provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,50 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import AppRouter from "components/Router";
 import AppRouter2 from "components/Router_HG";
 import axios from "axios";
+import { ProfileProvider } from "context/ProfileContext";
 
 const App = () => {
   const [init, setInit] = useState(false);
   const [userObj, setUserObj] = useState(true);
-  const [accessToken,setAccessToken] = useState('');
+  const [accessToken, setAccessToken] = useState("");
   const onLoggedIn = () => {
     setInit((prev) => !prev);
   };
-  
 
-  useEffect(()=>{
-    const userInfo = sessionStorage.getItem('user');
+  useEffect(() => {
+    const userInfo = sessionStorage.getItem("user");
 
-    if(!userInfo){
-      const getAccessToken = async (authorizationCode)=>{
-        await axios.post("https://api.cardbook.tk:4000/users/gitlogin",{
-          authorizationCode:authorizationCode
-        })
-        .then((token)=>{
-          onLoggedIn()
-          setAccessToken(token.data.accessToken)
-        })
-        .catch(()=>console.log("실패"));
-      }
-      const didmount = ()=>{
-        const url = new URL(window.location.href)
-        const authorizationCode = url.searchParams.get('code');      
-        if(authorizationCode){
-          getAccessToken(authorizationCode)
+    if (!userInfo) {
+      const getAccessToken = async (authorizationCode) => {
+        await axios
+          .post("https://api.cardbook.tk:4000/users/gitlogin", {
+            authorizationCode: authorizationCode,
+          })
+          .then((token) => {
+            onLoggedIn();
+            setAccessToken(token.data.accessToken);
+          })
+          .catch(() => console.log("실패"));
+      };
+      const didmount = () => {
+        const url = new URL(window.location.href);
+        const authorizationCode = url.searchParams.get("code");
+        if (authorizationCode) {
+          getAccessToken(authorizationCode);
         }
-      }
+      };
       didmount();
-    }else if(!setInit){
+    } else if (!setInit) {
       onLoggedIn();
     }
-  
-  },[])
+  }, []);
   return (
     <>
       {init ? (
-        <AppRouter isLoggedIn={userObj}/>
+        <ProfileProvider>
+          <AppRouter isLoggedIn={userObj} />
+        </ProfileProvider>
       ) : (
         <AppRouter2 onLoggedIn={onLoggedIn} />
       )}
